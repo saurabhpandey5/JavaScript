9cf7a3a2dc02e8087193f1c44d9f24196009eaef
saurabh.pandey3 <saurabh.pandey3@gmail.com>
2023-10-07 14:49:02 +0530

    Maths and Number

---
+++ Fundamental/String.js
@@ -0,0 +1,41 @@
+const name="saurabh";
+const repoCount=50;
+
+//console.log(name+repoCount+"value");
+
+// console.log(`hello my name is ${name} and my repo is ${repoCount}`);
+ 
+const  gameName=new String('Saurabh Pandey');
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4)); 
+
+// console.log(gameName.indexOf('b'));
+
+const newString=gameName.substring(0,4);
+console.log(newString);
+
+const newString1=gameName.trim(' ');
+console.log(newString1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Fundamental/nums_and_Math.js
@@ -0,0 +1,48 @@
+const score =400;
+// console.log(score);
+// console.log(typeof score);
+
+const balance= new  Number(123);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber=123.8966;
+// console.log(otherNumber.toPrecision(5));
+
+const hundreds=100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ************************** Maths *************
+// console.log(Math);
+// console.log(Math.abs(-2));
+
+// console.log(Math.round(2.3));
+// console.log(Math.random());
+
+// console.log(Math.ceil(2.3));
+// console.log(Math.floor(2.3));
+
+// console.log(Math.pow(2,3));
+
+// console.log((Math.random()*10)+1);
+// console.log((Math.floor(Math.random()*10)+1));
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
+
+
+
+
+
+
+
+
+
