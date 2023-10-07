55dae8e1d7c7c22f676ab52d63a435721148ea0f
saurabh.pandey3 <saurabh.pandey3@gmail.com>
2023-10-07 15:13:52 +0530

    Date And time

---
+++ Fundamental/Date.js
@@ -0,0 +1,44 @@
+//  Dates
+let myDate=new Date();
+// console.log(myDate); // 2023-10-07T09:23:43.932Z
+
+// console.log(myDate.toString()); // Sat Oct 07 2023 14:53:43 GMT+0530 (India Standard Time)
+
+// console.log(myDate.toDateString()); // Sat Oct 07 2023
+
+// console.log(myDate.toLocaleString()); // 10/7/2023, 2:55:20 PM
+
+//console.log(myDate.toISOString()); // 2023-10-07T09:25:56.104Z
+
+//console.log(myDate.toLocaleDateString()); // 10/7/2023
+
+// console.log(myDate.getDate());
+// console.log(myDate.getDay());
+// console.log(myDate.getFullYear());
+
+//console.log(typeof myDate);  //Object
+
+
+// let mycreateDate=new Date(2023,0,23);
+// let mycreateDate=new Date(2023,0,23,5,3);
+// console.log(mycreateDate.toLocaleString());
+
+let mydate1=new Date("01-14-2023");
+// console.log(mydate1.toLocaleString());
+
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+
+// console.log(mydate1.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate=new Date();
+// console.log(newDate.getDay());
+
+// console.log(newDate.toLocaleString('default,{weekday:"long"}'));
+
+
+
