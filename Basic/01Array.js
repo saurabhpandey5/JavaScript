e01f5f49c69948d5b4a93be0527b7cc9e7a78d69
saurabh.pandey3 <saurabh.pandey3@gmail.com>
2023-10-09 18:11:33 +0530

    Array

---
+++ Basic/01Array.js
@@ -0,0 +1,62 @@
+// array
+
+const myArr=[1,2,3,4,5,6];
+// console.log(myArr);
+// console.log(myArr[2]);
+
+const arr1=['hero1','hero2'];
+const arr2=new Array(1,2,3,4);
+// console.log(arr2);
+// console.log(arr2.length);
+
+// array methods
+
+const arr3=new Array(1,2,3,4);
+// arr3.push(5);
+// console.log(arr3);
+// arr3.pop();
+
+// arr3.unshift(9);
+// arr3.shift()
+
+// console.log(arr3.includes(3));
+
+// const newarr=arr3.join();
+// console.log(arr3);
+// console.log(newarr);
+
+
+//Slice ,splice
+
+console.log("A",arr3);
+const myn1=arr3.slice(1,3);
+console.log(myn1);
+console.log("B",arr3);
+const myn2=arr3.splice(1,3);
+console.log(myn2);
+console.log("C",arr3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Basic/02Array.js
@@ -0,0 +1,33 @@
+const marvel_hero=['thor','ironman','spiderman'];
+const dchero=['superman','flash','batman'];
+
+// marvel_hero.push(dchero); // it will push in the same array 
+// console.log(marvel_hero);
+// console.log(marvel_hero[3]);
+
+// const new_array=marvel_hero.concat(dchero); // it gives new array 
+// console.log(new_array);
+
+// const all_new=[...marvel_hero,...dchero]; // Spread
+// console.log(all_new);
+
+// const another=[1,2,3,4,5,[6,7,8,9],7,[2,3,4],5,[2,[34,3]]];
+// const realanother=another.flat(Infinity);
+// console.log(realanother);
+
+// console.log(Array.isArray("saurabh"));
+// console.log(Array.from("saurabh"));
+// console.log(Array.from({name:"saurabh"})); //gives Empty array
+
+let score=100;
+let score2=200;
+let score3=300;
+
+console.log(Array.of(score,score2,score3)); // Of will make array of multiple variable 
+
+
+
+
+
+
+
