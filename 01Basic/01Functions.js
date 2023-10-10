d5cd88b24f94b6213ecbaea41ba9591f68b6259e
saurabh.pandey3 <saurabh.pandey3@gmail.com>
2023-10-10 17:42:34 +0530

    function

---
+++ 01Basic/01Functions.js
@@ -0,0 +1,55 @@
+function sayMyName(){
+    console.log("saurabh");
+}
+// sayMyName();
+function addTwoNumbers(number1,number2){
+    return number1+number2;
+}
+
+// console.log(addTwoNumbers(5,null));
+
+function calculateCardPrice(...num1){
+   return num1;
+}
+
+// console.log(calculateCardPrice(200,400,500));
+
+const user={
+    userName:"saurabh",
+    price:230
+}
+
+function handleObject(anyObject){
+    console.log(`Usernme is ${anyObject.userName}, and price is ${anyObject.price}`)
+}
+// handleObject(user);
+
+// handleObject({
+//     userName:'Pandey',
+//     price:234
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
